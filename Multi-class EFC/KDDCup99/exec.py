2f9d1305a21c3de04ece2bc6b8d79c5b1c73d20f
munak98 <munak98@hotmail.com>
2021-12-01 08:38:05 -0300

    removing KDDCup99 stuff

--- Multi-class EFC/KDDCup99/exec.py
+++
@@ -1,8 +0,0 @@
-import os
-import numpy as np
-
-exec(open("split_validation.py").read())
-exec(open("process.py").read())
-exec(open("tuning.py").read())
-exec(open("train-test.py").read())
-exec(open("colect_results.py").read())
