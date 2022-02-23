d72512f893c588d9c267074f45ea6dabcb50e0db
munak98 <munak98@hotmail.com>
2022-02-23 14:29:00 -0300

    adapting ML_unknown to SVC

--- Multi-class EFC/CIDDS-001/ML_algorithms_unknown.py
+++ Multi-class EFC/CIDDS-001/ML_algorithms_unknown.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 import sys
 sys.path.append('../../../efc')
 from classification_functions import *
 import time
+import threading
 
 def RF(removed, sets):
     train = pd.read_csv("5-fold_sets/Normalized/Sets{}/X_train".format(sets), header=None)
@@ -25,6 +28,25 @@ def RF(removed, sets):
     print("RF test: ", time.time()-start)
     np.save("5-fold_sets/Results_removing{}/Sets{}/RF_predicted.npy".format(removed, sets), predict_labels)
 
+def SVC(removed, sets):
+    train = pd.read_csv("5-fold_sets/Normalized/Sets{}/X_train".format(sets), header=None)
+    train_labels = pd.read_csv("5-fold_sets/Normalized/Sets{}/y_train".format(sets), header=None)
+    test = pd.read_csv("5-fold_sets/Normalized/Sets{}/X_test".format(sets), header=None)
+
+    valid_indexes = np.where(train_labels == removed)[0]
+    train.drop(valid_indexes, axis=0, inplace=True)
+    train_labels.drop(valid_indexes, axis=0, inplace=True)
+
+    svc = SVC(kernel='poly')
+    start = time.time()
+    svc.fit(train, train_labels)
+    train_time = time.time()-start
+    start = time.time()
+    predict_labels = svc.predict(test)
+    test_time = time.time()-start
+    np.save("5-fold_sets/Results_removing{}/Sets{}/SVC_predicted.npy".format(removed, sets), predict_labels)
+    np.save("5-fold_sets/Results_removing{}/Sets{}/SVC_times.npy".format(removed, sets), [train_time, test_time])
+
 
 def EFC(removed, sets):
     test = pd.read_csv("5-fold_sets/Discretized/Sets{}/X_test".format(sets), header=None).astype('int')
@@ -38,16 +60,20 @@ def EFC(removed, sets):
 
     Q = 32
     LAMBDA = 0.5
-
+    start = time.time()
     h_i_matrices, coupling_matrices, cutoffs_list = MultiClassFit(np.array(train), np.array(train_labels), Q, LAMBDA)
+    train_time = time.time()-start
 
+    start = time.time()
     predicted = MultiClassPredict(np.array(test), h_i_matrices, coupling_matrices, cutoffs_list, Q, np.unique(train_labels))
+    test_time = time.time()-start
 
     np.save("5-fold_sets/Results_removing{}/Sets{}/EFC_predicted.npy".format(removed, sets), predicted)
+    np.save("5-fold_sets/Results_removing{}/Sets{}/EFC_times.npy".format(removed, sets), [train_time, test_time])
     print(classification_report(test_labels, predicted, labels=np.unique(test_labels)))
 
 
 for removed in [0, 1, 3, 4]:
     for sets in range(1,6):
-        EFC(removed, sets)
-        RF(removed, sets)
+        threading.Thread(EFC, args=(removed, sets,)).start()
+        threading.Thread(SVC, args=(removed, sets,)).start()
